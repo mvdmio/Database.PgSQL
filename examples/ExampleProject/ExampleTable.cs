28fa1b3a720553d2d72d7eda7fad6a76f5f0a212
Michiel van der Meer <contact@michielvandermeer.com>
2025-05-17 15:19:18 +0200

    Example project now also works

--- examples/ExampleProject/ExampleTable.cs
+++ examples/ExampleProject/ExampleTable.cs
@@ -1,10 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-using mvdmio.Database.PgSQL;
+﻿using mvdmio.Database.PgSQL;
+using mvdmio.Database.PgSQL.Attributes;
 
 namespace ExampleProject;
 
 [Table("example")]
-public class ExampleTable : DbTable
+public partial class ExampleTable : DbTable
 {
    [Column("id")]
    public long Id { get; set; }
