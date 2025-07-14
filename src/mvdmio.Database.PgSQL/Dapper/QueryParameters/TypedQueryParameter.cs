ba0002b5eb0bdef837ab40aebda075a225e79a88
Michiel van der Meer <contact@michielvandermeer.com>
2025-07-14 22:40:10 +0200

    Added TypedQueryParameter

---
+++ src/mvdmio.Database.PgSQL/Dapper/QueryParameters/TypedQueryParameter.cs
@@ -0,0 +1,36 @@
+﻿using System.Data;
+using Dapper;
+using JetBrains.Annotations;
+using Npgsql;
+using NpgsqlTypes;
+
+namespace mvdmio.Database.PgSQL.Dapper.QueryParameters;
+
+/// <summary>
+///   Custom query parameter for setting PostgreSQL parameter types.
+/// </summary>
+[PublicAPI]
+public class TypedQueryParameter : SqlMapper.ICustomQueryParameter
+{
+   private readonly object? _value;
+   private readonly NpgsqlDbType _dbType;
+
+   /// <summary>
+   /// Constructor.
+   /// </summary>
+   public TypedQueryParameter(object? value, NpgsqlDbType dbType)
+   {
+      _value = value;
+      _dbType = dbType;
+   }
+
+   /// <inheritdoc />
+   public void AddParameter(IDbCommand command, string name)
+   {
+      var parameter = new NpgsqlParameter(name, _dbType) {
+         Value = _value
+      };
+
+      command.Parameters.Add(parameter);
+   }
+}
