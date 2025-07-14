0ef7dd338bf905ab367f57e457923795a218e3b5
Michiel van der Meer <mvdmeer@jewelsoftware.com>
2025-07-14 09:43:23 +0200

    Added possibility to add where clause to on-conflict in Bulk Upsert

--- src/mvdmio.Database.PgSQL/Connectors/BulkConnector.cs
+++ src/mvdmio.Database.PgSQL/Connectors/BulkConnector.cs
@@ -91,50 +91,81 @@ public class BulkConnector
    /// <summary>
    ///   Do an upsert on a table. Creates a temp-table, copies the data to it and then does an upsert on the original table.
    /// </summary>
-   /// <param name="tableName">The table to do the upsert on.</param>
+   /// <param name="tableName">The table to do the upsert on</param>
    /// <param name="onConflictColumn">The on-conflict column</param>
-   /// <param name="items">The data to upsert.</param>
+   /// <param name="items">The data to upsert</param>
    /// <param name="columnValueMapping">The data-to-column mapping</param>
-   /// <param name="ignoreErrors">Flag for ignoring errors. Will not throw exceptions when enabled.</param>
+   /// <param name="ignoreErrors">Flag for ignoring errors. Will not throw exceptions when enabled</param>
+   /// <param name="ct">The cancellation token</param>
    /// <typeparam name="T">The Type of the data to insert</typeparam>
    /// <returns>A task for asynchronous awaiting.</returns>
    public Task UpsertAsync<T>(string tableName, string onConflictColumn, IEnumerable<T> items, Dictionary<string, Func<T, DbValue>> columnValueMapping, bool ignoreErrors = false, CancellationToken ct = default)
    {
       return UpsertAsync(tableName, [ onConflictColumn ], items, columnValueMapping, ignoreErrors, ct);
    }
-   
+
    /// <summary>
    ///   Do an upsert on a table. Creates a temp-table, copies the data to it and then does an upsert on the original table.
    /// </summary>
-   /// <param name="tableName">The table to do the upsert on.</param>
+   /// <param name="tableName">The table to do the upsert on</param>
    /// <param name="onConflictColumns">The on-conflict columns</param>
-   /// <param name="items">The data to upsert.</param>
+   /// <param name="items">The data to upsert</param>
    /// <param name="columnValueMapping">The data-to-column mapping</param>
-   /// <param name="ignoreErrors">Flag for ignoring errors. Will not throw exceptions when enabled.</param>
+   /// <param name="ignoreErrors">Flag for ignoring errors. Will not throw exceptions when enabled</param>
+   /// <param name="ct">The cancellation token</param>
+   /// <typeparam name="T">The Type of the data to insert</typeparam>
+   /// <returns>A task for asynchronous awaiting.</returns>
+   public Task UpsertAsync<T>(string tableName, string[] onConflictColumns, IEnumerable<T> items, Dictionary<string, Func<T, DbValue>> columnValueMapping, bool ignoreErrors = false, CancellationToken ct = default)
+   {
+      return UpsertAsync(
+         tableName,
+         new UpsertConfiguration {
+            OnConflictColumns = onConflictColumns
+         },
+         items,
+         columnValueMapping,
+         ignoreErrors,
+         ct
+      );
+   }
+
+   /// <summary>
+   ///   Do an upsert on a table. Creates a temp-table, copies the data to it and then does an upsert on the original table.
+   /// </summary>
+   /// <param name="tableName">The table to do the upsert on</param>
+   /// <param name="upsertConfiguration">The upsert configuration to use</param>
+   /// <param name="items">The data to upsert</param>
+   /// <param name="columnValueMapping">The data-to-column mapping</param>
+   /// <param name="ignoreErrors">Flag for ignoring errors. Will not throw exceptions when enabled</param>
+   /// <param name="ct">The cancellation token</param>
    /// <typeparam name="T">The Type of the data to insert</typeparam>
    /// <returns>A task for asynchronous awaiting.</returns>
    [SuppressMessage("ReSharper", "PossibleMultipleEnumeration", Justification = "Multiple enumerations are not a problem here.")]
-   public async Task UpsertAsync<T>(string tableName, string[] onConflictColumns, IEnumerable<T> items, Dictionary<string, Func<T, DbValue>> columnValueMapping, bool ignoreErrors = false, CancellationToken ct = default)
+   public async Task UpsertAsync<T>(string tableName, UpsertConfiguration upsertConfiguration, IEnumerable<T> items, Dictionary<string, Func<T, DbValue>> columnValueMapping, bool ignoreErrors = false, CancellationToken ct = default)
    {
       if (!items.Any())
          return;
 
       var tempTableName = $"temp_{Guid.NewGuid():N}";
       var allColumns = columnValueMapping.Keys;
-      var updateColumns = allColumns.Where(x => !onConflictColumns.Contains(x)).ToArray();
+      var updateColumns = allColumns.Where(x => !upsertConfiguration.OnConflictColumns.Contains(x)).ToArray();
 
       await _db.InTransactionAsync(
          async () => {
-            await _db.Dapper.ExecuteAsync($"CREATE TEMP TABLE {tempTableName} (LIKE {tableName} INCLUDING DEFAULTS INCLUDING IDENTITY);");
+            await _db.Dapper.ExecuteAsync($"CREATE TEMP TABLE {tempTableName} (LIKE {tableName} INCLUDING CONSTRAINTS INCLUDING DEFAULTS INCLUDING GENERATED INCLUDING IDENTITY);");
 
             await CopyAsync(tempTableName, items, columnValueMapping, ignoreErrors, ct);
 
+            var onConflictWhereClause = string.IsNullOrEmpty(upsertConfiguration.OnConflictWhereClause)
+               ? string.Empty
+               : $"WHERE {upsertConfiguration.OnConflictWhereClause}";
+            
             await _db.Dapper.ExecuteAsync(
                $"""
                 INSERT INTO {tableName} ({string.Join(", ", allColumns)})
                 SELECT {string.Join(", ", allColumns)}
                 FROM {tempTableName}
-                ON CONFLICT ({string.Join(", ", onConflictColumns)}) DO UPDATE
+                ON CONFLICT ({string.Join(", ", upsertConfiguration.OnConflictColumns)}) {onConflictWhereClause } DO UPDATE
                 SET {string.Join(", ", updateColumns.Select(x => $"{x} = EXCLUDED.{x}"))}
                 """
             );
---
+++ src/mvdmio.Database.PgSQL/Connectors/UpsertConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using JetBrains.Annotations;
+
+namespace mvdmio.Database.PgSQL.Connectors;
+
+/// <summary>
+///   Configuration options for the <see cref="BulkConnector.UpsertAsync" /> method.
+/// </summary>
+[PublicAPI]
+public sealed class UpsertConfiguration
+{
+   /// <summary>
+   ///   The on-conflict columns to use.
+   /// </summary>
+   public required string[] OnConflictColumns { get; init; }
+   
+   /// <summary>
+   ///   The on-conflict where clause to use. This is used to filter the rows that are updated in the case of a conflict. Must NOT start with "WHERE", this is added automatically.
+   /// </summary>
+   public string OnConflictWhereClause { get; init; } = string.Empty;
+}
